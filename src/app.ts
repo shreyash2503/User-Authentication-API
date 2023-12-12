074e13ab4a5e8cd17f58ad1ee06fdd6ad7957168
Shreyash <shreyashtekade2503@gmail.com>
2023-12-12 22:01:38 +0530

    created the user model

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import config from "config";
 import connect from "./utils/connect";
+import log from "./utils/logger";
+import routes from "./routes";
 
 const port = config.get<number>("port");
 
 const app = express();
 
 app.listen(port, async () => {
-  console.log("Server running on port 9000");
+  log.info(`Server started at ${port}`);
   await connect();
+
+  routes(app);
 });
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,15 +1,15 @@
 import mongoose, { mongo } from "mongoose";
 import config from "config";
+import log from "./logger";
 
 async function connect(): Promise<void> {
   const dbUri = config.get<string>("dbUri");
 
   try {
     await mongoose.connect(dbUri);
-    console.log("Connected to database");
+    log.info("Connected to database");
   } catch (e) {
-    console.log("Error connecting to database");
-    console.log(e);
+    log.error("Error connecting to database");
     process.exit(1);
   }
 }
